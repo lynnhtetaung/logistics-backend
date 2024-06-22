1a856939abb7c66d2a25929ba2b7b36e553b6895
Lynn Htet Aung <lynnhtetaung4@gmail.com>
2024-06-23 02:07:51 +0900

    added vue-sso-project to be the same port with express

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var trackingsRouter = require('./routes/tracking');
 
 var app = express();
 
+// Serve static files from the Vue.js build directory
+app.use(express.static(path.join(__dirname, 'vue-sso-project/dist')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -30,6 +33,11 @@ app.use(bodyParser.json());
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/trackings', trackingsRouter);
+
+// Serve the Vue.js app for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'vue-sso-project/dist/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
